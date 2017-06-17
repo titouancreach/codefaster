46a6c06f9c13b868fd1ec0baa815882b4ae2737c
Titouan CREACH <titouan.creach@eegle.io>
2017-06-17 14:59:55 +0200

    why decide to make a gui since the terminal itself can read input ? remove curses btw

--- src/codefaster.py
+++ src/codefaster.py
@@ -1,18 +1,13 @@
-from functools import partial
-
 import argparse
-import curses
+import config
 import glob
 import itertools
 import logging
 import os.path
 import random
-import config
 
 logging.basicConfig(level=config.LOGGING_LEVEL,
                     format='%(asctime)s -- %(message)s')
-
-AUTHORIZED_INPUT = list(range(32, 126))
 
 
 def scan_basedir(basedir, fileext):
@@ -66,58 +61,6 @@ def randomlinevalid(files_path):
     return line
 
 
-def configui(stdscr):
-    """
-    Configure the UI.
-        :param stdscr curse screen handle
-    """
-    curses.noecho()
-    curses.nocbreak()
-    stdscr.keypad(True)
-
-
-def mainloop(stdscr, win, linebox, inputbox, files_path):
-    """
-    Manage events
-        :param stdscr curse screen handle
-        :param win curse main window handle
-        :param linebox curse box handle where the line is displayed
-        :param inputbox curse box handle where the input is displayed
-        :param files_path the file list where we fetch the line
-    """
-
-    line = randomlinevalid(files_path)
-
-    linebox.addstr(1, 1, line)
-
-    inputbox.move(1, 1)
-    inputbox.nodelay(True)
-
-    linebox.refresh()
-    inputbox.refresh()
-
-    inputline = ''
-
-    try:
-        while True:
-            c = inputbox.getch()
-
-            if c != -1 and c in AUTHORIZED_INPUT:
-                inputline += chr(c)
-                inputbox.erase()
-                inputbox.box()
-                inputbox.addstr(1, 1, inputline)
-                inputbox.move(1, len(inputline))
-
-                stdscr.refresh()
-                win.refresh()
-                inputbox.refresh()
-                linebox.refresh()
-
-    except KeyboardInterrupt:
-        pass
-
-
 def cleanline(line):
     """
     Clean the line, replace tabs by space, remove trailings spaces
@@ -135,32 +78,6 @@ def lineisvalid(line):
     return line != ''
 
 
-def startui(files_path, stdscr):
-    """
-    Start the UI mode.
-    The screen handle is created by the wrapper and is
-    automatically bound to this function.
-        :param stdscr curse screen handle
-    """
-    configui(stdscr)
-
-    win = curses.newwin(curses.LINES - 1, curses.COLS - 1, 0, 0)
-    win.box()
-
-    linebox = curses.newwin(3, curses.COLS - 3, 1, 1)
-    linebox.box()
-
-    inputbox = curses.newwin(3, curses.COLS - 3, 4, 1)
-    inputbox.box()
-
-    stdscr.refresh()
-    win.refresh()
-    inputbox.refresh()
-    linebox.refresh()
-
-    mainloop(stdscr, win, linebox, inputbox, files_path)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Code faster')
     parser.add_argument('basedir', metavar='basedir', type=str,
@@ -174,10 +91,6 @@ if __name__ == '__main__':
     filenames = list(filespath)
     logging.info('Finished, {} files where found...'.format(len(filenames)))
     logging.info('Starting UI')
-
-    f = partial(startui, filenames)
-
-    curses.wrapper(f)
 
     logging.info('bye !')
 
